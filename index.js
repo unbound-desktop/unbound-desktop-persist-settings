881dd5ae146f802832a4d58787b9163a4e4dc24a
eternal <eternal.git@gmail.com>
2022-03-07 02:38:54 +0000

    use change listeners instead

--- index.js
+++ index.js
@@ -4,23 +4,17 @@ const { bulk, filters: { byProps } } = require('@webpack');
 const { Dispatcher } = require('@webpack/common');
 const { bindAll } = require('@utilities');
 
-const {
-   AccessiblityEvents,
-   VoiceEvents,
-   NotificationEvents
-} = require('./constants');
-
 const [
    experiments,
-   notifications,
    accessibility,
+   notifications,
    storage,
    keybinds,
    voice
 ] = bulk(
    byProps('hasRegisteredExperiment'),
-   byProps('getDesktopType'),
    byProps('isZoomedIn'),
+   byProps('getDesktopType'),
    byProps('ObjectStorage'),
    byProps('hasKeybind'),
    byProps('isDeaf')
@@ -42,49 +36,25 @@ module.exports = class PersistSettings extends Plugin {
    }
 
    start() {
+      accessibility.addChangeListener(this.backupAccessibility);
+      notifications.addChangeListener(this.backupNotifications);
+      experiments.addChangeListener(this.backupKeybinds);
+      keybinds.addChangeListener(this.backupKeybinds);
+      voice.addChangeListener(this.backupVoice);
+
       Dispatcher.subscribe('CONNECTION_OPEN', this.restore);
-      Dispatcher.subscribe('KEYBINDS_ADD_KEYBIND', this.backupKeybinds);
-      Dispatcher.subscribe('KEYBINDS_SET_KEYBIND', this.backupKeybinds);
-      Dispatcher.subscribe('USER_SETTINGS_UPDATE', this.backupSettings);
-      Dispatcher.subscribe('KEYBINDS_DELETE_KEYBIND', this.backupKeybinds);
-      Dispatcher.subscribe('KEYBINDS_ENABLE_ALL_KEYBINDS', this.backupKeybinds);
-      Dispatcher.subscribe('EXPERIMENT_OVERRIDE_BUCKET', this.backupExperiments);
-
-      for (const event of AccessiblityEvents) {
-         Dispatcher.subscribe(event, this.backupAccessibility);
-      }
-
-      for (const event of VoiceEvents) {
-         Dispatcher.subscribe(event, this.backupVoice);
-      }
-
-      for (const event of NotificationEvents) {
-         Dispatcher.subscribe(event, this.backupNotifications);
-      }
 
       setTimeout(() => this.didRestore || this.restore(), 1000 * 10);
    }
 
    stop() {
       Dispatcher.unsubscribe('CONNECTION_OPEN', this.restore);
-      Dispatcher.unsubscribe('KEYBINDS_ADD_KEYBIND', this.backupKeybinds);
-      Dispatcher.unsubscribe('KEYBINDS_SET_KEYBIND', this.backupKeybinds);
-      Dispatcher.unsubscribe('USER_SETTINGS_UPDATE', this.backupSettings);
-      Dispatcher.unsubscribe('KEYBINDS_DELETE_KEYBIND', this.backupKeybinds);
-      Dispatcher.unsubscribe('KEYBINDS_ENABLE_ALL_KEYBINDS', this.backupKeybinds);
-      Dispatcher.unsubscribe('EXPERIMENT_OVERRIDE_BUCKET', this.backupExperiments);
 
-      for (const event of AccessiblityEvents) {
-         Dispatcher.unsubscribe(event, this.backupAccessibility);
-      }
-
-      for (const event of VoiceEvents) {
-         Dispatcher.unsubscribe(event, this.backupVoice);
-      }
-
-      for (const event of NotificationEvents) {
-         Dispatcher.unsubscribe(event, this.backupNotifications);
-      }
+      accessibility.removeChangeListener(this.backupAccessibility);
+      notifications.removeChangeListener(this.backupNotifications);
+      experiments.removeChangeListener(this.backupKeybinds);
+      keybinds.removeChangeListener(this.backupKeybinds);
+      voice.removeChangeListener(this.backupVoice);
    }
 
    backupKeybinds() {
